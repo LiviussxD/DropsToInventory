bfdfef0c29a5c0034d2c90c96f562722062a08e3
LiviussxD <52212825+LiviussxD@users.noreply.github.com>
2019-06-25 23:21:15 +0700

    Update CommandManager.java

--- src/net/amunak/bukkit/dropstoinventory/CommandManager.java
+++ src/net/amunak/bukkit/dropstoinventory/CommandManager.java
@@ -1,22 +1,5 @@
 package net.amunak.bukkit.dropstoinventory;
 
-/**
- * Copyright 2014 Eric Coan
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -26,8 +9,8 @@ import org.bukkit.entity.Player;
 import java.util.List;
 
 /**
- * Creator: Revolution
- * Date: 7/9/14.
+ * Creator: LiviussxD
+ * Date: 26/6/2019
  * Project: DropsToInventory
  * Usage: Manages Commands
  */
